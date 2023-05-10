d5cc84f994dbeb20fec561909ddec224b16d12cb
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-05-10 11:06:33 +0100

    A

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num != 99:
-        print("{:02}".format(number), end=", ")
+        print("{:02}".format(num), end=", ")
     elif num == 99:
-        print("{}".format(number))
+        print("{}".format(num))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num_1 in range(0, 10):
     for num_2 in range(num_1 + 1, 10):
         if num_1 != 8:
-            print("{}{}".format(num_1, num_2), end= ", ")
+            print("{}{}".format(num_1, num_2), end=", ")
         elif num_1 == 8:
             print("{}{}".format(num_1, num_2))
