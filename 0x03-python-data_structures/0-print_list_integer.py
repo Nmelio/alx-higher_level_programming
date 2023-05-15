f4591b22608fa4802664c2ce4a54913c55ba5fb7
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-05-15 09:14:26 -0700

    A

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    for x in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - function that prints basic info about Python lists
+ *
+ * @p: A Python Object list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int aa, bb, cc;
+	PyObject *dd;
+
+	aa = Py_SIZE(p);
+	bb = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", sizet);
+	printf("[*] Allocated = %d\n", bb);
+
+	for (cc = 0; cc < size; cc++)
+	{
+		printf("Element %d: ", cc);
+
+		dd = PyList_GetItem(p, cc);
+		printf("%s\n", Py_TYPE(dd)->tp_name);
+	}
+}
