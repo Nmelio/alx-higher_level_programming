3c8cb79ac525c98e1370f5504957550db26d7a31
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-05-29 15:12:54 +0100

    A

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+
+def magic_calculation(a, b):
+    t = 0
+    for u in range(1, 3):
+        try:
+            if u > a:
+                raise Exception('Too far')
+            else:
+                t += a ** b / u
+        except Exception:
+            t = b + a
+            break
+        return (t)
